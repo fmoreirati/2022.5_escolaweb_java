1c60e9e2037aac6992984255cceb2fccb3fa1561
fmoreirati <fabianomoreira.ti@gmail.com>
2023-03-29 18:36:52 -0300

    teste

--- src/br/com/escolaweb/services/AlunoService.java
+++ src/br/com/escolaweb/services/AlunoService.java
@@ -1,13 +1,22 @@
 package br.com.escolaweb.services;
 
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 import br.com.escolaweb.model.Aluno;
 
 public class AlunoService extends DAO {
 
-    public void add(Aluno aluno) {
+    public void add(Aluno aluno) throws SQLException {
+        PreparedStatement stman = null;
         String sql1 = "insert into pessoa (_id, nome, email, data_nasc, senha) values (?,?,?,?,?)";
         String sql2 = "insert into aluno (matricula) values (?)";
-        Conectar();
 
+        Conectar();
+        stman = conn.prepareStatement(sql1);
+        stman.setString(1, aluno.getNome());
+        stman.execute();
+        stman.close();
+        conn.close();
     }
 }
